7f9c94872c09ceeb7098a139608de34c1ad01aa9
unknown <jdai315@gmail.com>
2014-10-18 12:27:21 -0400

    added skeleton files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Blog
 ====
+Period 5
+
+David Bang
+Jerry Dai
+Vanessa Yan
+Cindy Wu
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+#homepage
+@app.route("/", methods=["GET","POST"])
+def index(txt = None):
+    if request.method == "GET":
+        return render_template("index.html", txt = None)
+    else:
+        txtbox = request.form["input"]
+        button = request.form["enter"]
+        if button == None:
+            return render_template("index.html", txt = None)
+        else:
+            return render_template("title.html", txt = txtbox)
+
+
+#main
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=2014)
