2ef9fe4f8cb9b2c875646d7b02b5bae06278c432
Vanessa <yu100997@gmail.com>
2014-10-21 11:55:40 -0400

    idk

--- app.py
+++ app.py
@@ -5,7 +5,20 @@ app = Flask(__name__)
 #homepage
 @app.route("/")
 def index(txt = None):
-    render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/blog/<title>")
+def title(title=None):
+    blogtitle = request.args.get(blogtitle)
+    blogtext = request.args.get(blogtext)
+    submit = request.args.get(submit)
+    if (submit == "Submit" and blogtext != ""):
+        if (blogtitle == ""):
+            blogtitle = "Untitled Post"
+        return render_template("title.html", title = title)
+    else:
+        return render_template("title.html", title=title)
+    
     
 if __name__ == "__main__":
     app.debug = True
