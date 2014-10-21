178816f85e032d1321ae64773a46a320ce268c6a
Jerry Dai <jdai315@gmail.com>
2014-10-21 11:55:06 -0400

    database stuffs

--- create.py
+++ create.py
@@ -4,21 +4,22 @@ import csv
 conn = sqlite3.connect("posts.db")
 c = conn.cursor()
 
-q = "create table posts(name text, id integer)"
+q = "create table posts(id integer, name text)"
 c.execute(q)
-q = "create table comments(name text, body text)"
+q = "create table comments(id integer, name text, body text)"
 c.execute(q)
 
-BASE = "INSERT INTO posts VALUES('%(name)s', %(id)s)"
-for line in csv.DictReader(open("posts.csv")):
-    q = BASE%line
-    print q
-    c.execute(q)
 
-BASE = "INSERT INTO comments VALUES('%(name)s', '%(body)s')"
-for line in csv.DictReader(open("comments.csv")):
-    q = BASE%line
-    print q
-    c.execute(q)
+#BASE = "INSERT INTO posts VALUES(%(id)s, '%(name)s')"
+#for line in csv.DictReader(open("posts.csv")):
+#    q = BASE%line
+#    print q
+#    c.execute(q)
+#
+#BASE = "INSERT INTO comments VALUES(%(id)s, '%(name)s', '%(body)s')"
+#for line in csv.DictReader(open("comments.csv")):
+#    q = BASE%line
+#    print q
+#    c.execute(q)
 
 conn.commit();
---
+++ posting.py
@@ -0,0 +1,32 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect("posts.db")
+c = conn.cursor()
+
+def post_blog(id, name):
+    s = "INSERT INTO posts VALUES(" + str(id) + ",'" + name + "')"
+    c.execute(s)
+    conn.commit();
+    
+def post_comment(id, name, body):
+    s = "INSERT INTO comments VALUES(" + str(id) + ",'" + name + "','" + body + "')"
+    c.execute(s)
+    conn.commit();
+
+def get_blog(id):
+    s = "SELECT name FROM posts WHERE id = " + str(id)
+    c.execute(s)
+    conn.commit();
+    b = c.fetchall()
+    print b
+    return b
+
+def get_comment(id):
+    s = "SELECT name FROM comments WHERE id = " + str(id)
+    c.execute(s)
+    conn.commit();
+    b = c.fetchall()
+    print b
+    return b
+
