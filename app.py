ccfea45a9e42385323aa1a5d62b72bda7241dc02
Vanessa <yu100997@gmail.com>
2014-10-21 11:24:36 -0400

    turned to jinja template

--- app.py
+++ app.py
@@ -3,19 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 #homepage
-@app.route("/", methods=["GET","POST"])
+@app.route("/")
 def index(txt = None):
-    if request.method == "GET":
-        return render_template("home.html", txt = None)
-    else:
-        txtbox = request.form["input"]
-        button = request.form["enter"]
-        if button == None:
-            return render_template("index.html", txt = None)
-        else:
-            return render_template("home.html", txt = txtbox)
-        
-#main
+    render_template("index.html")
+    
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0", port=2014)
