95e8518f7fde20fdfedb537ce11863e5b1e14bd7
Cindy <cindyofwu@gmail.com>
2014-10-22 00:43:51 -0400

    blogtitle/blogtext

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, request
 
+import posting
+
 app = Flask(__name__)
 
 #homepage
 @app.route("/")
-def index(txt = None):
+def index():
     return render_template("index.html")
 
+@app.route("/blog")
 @app.route("/blog/<title>")
-def title(title=None):
-    blogtitle = request.args.get(blogtitle)
-    blogtext = request.args.get(blogtext)
-    submit = request.args.get(submit)
+def title(title=None, text=None):
+    blogtitle = request.args.get("blogtitle")
+    blogtext = request.args.get("blogtext")
+    submit = request.args.get("submit")
     if (submit == "Submit" and blogtext != ""):
         if (blogtitle == ""):
             blogtitle = "Untitled Post"
-        return render_template("title.html", title = title)
+        return render_template("title.html", title=blogtitle, text=blogtext)
     else:
-        return render_template("title.html", title=title)
+        return render_template("title.html", title=blogtitle, text=blogtext)
     
     
 if __name__ == "__main__":
